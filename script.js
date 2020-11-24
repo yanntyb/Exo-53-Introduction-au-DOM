4dac5fff9dce4537a23f8b0965a8c275a9e18072
yann tyb <yanntyb.lbc@gmail.com>
2020-11-24 15:55:51 +0100

    a

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-document.getElementById("para1").style.color = "red";
-document.getElementById("para2").style.color = "red";
-document.getElementById("para3").style.color = "red";
-document.getElementById("para4").style.color = "red";
-document.getElementById("para5").style.color = "red";
+para = document.getElementsByTagName("p");
+
+for(let i = 0; i < 5; i++){
+    para[i].style.color = "red"
+}
+
+para[0].style.width = "300px"
 
 span = document.getElementsByTagName("span");
 
 for(let i=0; i < 10; i++){
     span[i].style.backgroundColor = "red";
+    span[i].style.display = "block"
+    span[i].dir = "rtl";
 }
 
-document.getElementById("para1").style.animationName = "animText";
-document.getElementById("para1").style.animationDuration= "2s";
+para[0].style.animationName = "animText";
+para[0].style.animationDuration= "5s";
+para[0].style.animationDirection= "alternate"
+para[0].style.animationIterationCount= "infinite";
 
 lien = document.getElementsByTagName("a");
 
 for(let i=0; i < 5; i++){
-    lien[i].innerHTML += i
+    lien[i].innerHTML = "lien " + i
 }
 
-span[0].dir = "rtl"
+
+
 
 document.getElementById("para5").innerHTML = "text"
